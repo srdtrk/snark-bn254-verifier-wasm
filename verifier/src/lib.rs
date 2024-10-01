50c7434c58bd75407b330d6cc90c60041f39d693
c-node <chuckhead7@gmail.com>
2024-09-30 18:28:19 -0700

    it builds

--- verifier/src/lib.rs
+++ verifier/src/lib.rs
@@ -13,6 +13,7 @@ use plonk::{
     error::PlonkError, load_plonk_proof_from_bytes, load_plonk_verifying_key_from_bytes,
     verify_plonk,
 };
+use wasm_bindgen::prelude::*;
 
 mod constants;
 mod converter;
@@ -22,52 +23,12 @@ mod hash_to_field;
 mod plonk;
 mod transcript;
 
-/// A verifier for Groth16 zero-knowledge proofs.
-#[derive(Debug)]
-pub struct Groth16Verifier;
+#[wasm_bindgen]
+/// WASM to verify a groth16 proof
+pub fn verify_groth16_wasm(proof: &[u8], vk: &[u8], public_inputs: &[u8]) -> bool {
+    let frs: Vec<Fr> = public_inputs.chunks(8).map(|slice| Fr::from_slice(slice).unwrap()).collect();
+    let proof = load_groth16_proof_from_bytes(proof).unwrap();
+    let vk = load_groth16_verifying_key_from_bytes(vk).unwrap();
 
-impl Groth16Verifier {
-    /// Verifies a Groth16 proof.
-    ///
-    /// # Arguments
-    ///
-    /// * `proof` - The proof bytes.
-    /// * `vk` - The verification key bytes.
-    /// * `public_inputs` - The public inputs.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` containing a boolean indicating whether the proof is valid,
-    /// or a `Groth16Error` if verification fails.
-    pub fn verify(proof: &[u8], vk: &[u8], public_inputs: &[Fr]) -> Result<bool, Groth16Error> {
-        let proof = load_groth16_proof_from_bytes(proof).unwrap();
-        let vk = load_groth16_verifying_key_from_bytes(vk).unwrap();
-
-        verify_groth16(&vk, &proof, public_inputs)
-    }
-}
-
-/// A verifier for Plonk zero-knowledge proofs.
-#[derive(Debug)]
-pub struct PlonkVerifier;
-
-impl PlonkVerifier {
-    /// Verifies a Plonk proof.
-    ///
-    /// # Arguments
-    ///
-    /// * `proof` - The proof bytes.
-    /// * `vk` - The verification key bytes.
-    /// * `public_inputs` - The public inputs.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` containing a boolean indicating whether the proof is valid,
-    /// or a `PlonkError` if verification fails.
-    pub fn verify(proof: &[u8], vk: &[u8], public_inputs: &[Fr]) -> Result<bool, PlonkError> {
-        let proof = load_plonk_proof_from_bytes(proof).unwrap();
-        let vk = load_plonk_verifying_key_from_bytes(vk).unwrap();
-
-        verify_plonk(&vk, &proof, public_inputs)
-    }
+    verify_groth16(&vk, &proof, &frs).is_ok()
 }
